57b5a546e6eac50787aa2fd5888c086e9dff07c9
ImdeukCho <108943478+ImdeukCho@users.noreply.github.com>
2022-07-24 20:10:04 +0900

    Add files via upload

---
+++ java/05Day_6_CLASS/src/Student.java
@@ -0,0 +1,13 @@
+
+
+// 클래스 이름 Student == 파일명,첫문자 대문자
+public class Student { // 모델클래스 학생한명 객체를 생성, 저장한 모델클래스
+	
+
+	String name="홍길동"; 
+	int age=10; // 나이
+	String address="서울"; 
+	
+	// 기본생성자 자동추가
+	// public Student(){}
+} // end class
