574dd9fcbfc0a5c3411ae01ae29928fb1879e40f
ImdeukCho <108943478+ImdeukCho@users.noreply.github.com>
2022-08-10 23:08:55 +0900

    Add files via upload

---
+++ java/JavaWorkshop00/src/MyTest02.java
@@ -0,0 +1,11 @@
+
+public class MyTest02 {
+
+	public static void main(String[] args) {
+		long ssn = 9010081654824L;
+		String ssn2 = "901008-1654824";
+		System.out.println("나의 주민번호:" + ssn);
+		System.out.println("나의 주민번호:" + ssn2);
+	}
+
+}
