908c79d058338e2896fddf3c86bfb4c770dac2df
ImdeukCho <108943478+ImdeukCho@users.noreply.github.com>
2022-08-15 23:05:23 +0900

    Add files via upload

---
+++ servlet,jsp/03_Servlet_파라미터_getParameter_로그인,조건X/src/main/java/com/controller/LoginServlet.java
@@ -0,0 +1,43 @@
+package com.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/loginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+ 
+    public LoginServlet() {
+        super();
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	// 1. post 한글처리
+			request.setCharacterEncoding("utf-8");
+			// 요청작업 처리
+			String userid = request.getParameter("userid");
+			String passwd = request.getParameter("passwd");
+			
+			// 응답처리 
+			response.setContentType("text/html;charset=utf-8");
+			PrintWriter out = response.getWriter();
+			out.print("<html><body>");
+			out.print("이름은:"+userid);
+			out.print("비번은:"+passwd);
+			out.print("</body></html>");
+			
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+}
