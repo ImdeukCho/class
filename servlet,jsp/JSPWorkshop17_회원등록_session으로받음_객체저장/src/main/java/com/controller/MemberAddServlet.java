02f67d01021978e8d1a59b3a06ea030892770385
ImdeukCho <108943478+ImdeukCho@users.noreply.github.com>
2022-08-22 21:54:06 +0900

    Add files via upload

---
+++ servlet,jsp/JSPWorkshop17_회원등록_session으로받음_객체저장/src/main/java/com/controller/MemberAddServlet.java
@@ -0,0 +1,51 @@
+package com.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.dto.MemberDTO;
+
+@WebServlet("/MemberAddServlet")
+public class MemberAddServlet extends HttpServlet {
+
+ 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+ 		request.setCharacterEncoding("utf-8");
+ 		
+ 		
+ 		
+ 		String username = request.getParameter("username");
+ 		int age = Integer.parseInt(request.getParameter("age"));
+ 		String address = request.getParameter("address");
+ 		
+ 		MemberDTO dto = new MemberDTO(username, age, address);		// dto 객체에 저장
+ 		
+ 		// 세션 생성
+ 		HttpSession session = request.getSession();
+ 		session.setAttribute("user", dto);
+ 		
+ 		
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		
+ 		out.print("<html>");
+ 		out.print("<body>");
+ 		out.print("<h2>세션에 이름/나이/주소 저장 성공</h2>");
+ 		out.print("<a href ='MemberListServlet'>세션정보 보기</a>");
+ 		out.print("</body>");
+ 		out.print("</html>");
+ 		
+ 		
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
